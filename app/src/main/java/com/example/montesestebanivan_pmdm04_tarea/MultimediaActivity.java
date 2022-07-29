3361c471b8a65acda49b4bf648bfdf9dfc29d6ca
Iván <87393011+Ivan-Montes@users.noreply.github.com>
2022-07-29 21:05:46 +0200

    Update MultimediaActivity.java
    According CodeFactor checking

--- app/src/main/java/com/example/montesestebanivan_pmdm04_tarea/MultimediaActivity.java
+++ app/src/main/java/com/example/montesestebanivan_pmdm04_tarea/MultimediaActivity.java
@@ -42,8 +42,15 @@ public class MultimediaActivity extends AppCompatActivity {
 
     public void onDestroy() {
         super.onDestroy();
-        if (mediaPlayer != null) {mediaPlayer.stop();mediaPlayer.release();mediaPlayer=null;};
-        if (videoView != null) {videoView.stopPlayback();videoView = null;};
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer=null;
+        }
+        if (videoView != null) {
+            videoView.stopPlayback();
+            videoView = null;
+        }
     }
 
 
